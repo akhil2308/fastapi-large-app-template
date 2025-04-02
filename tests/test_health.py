e4006cf8ebe8e09fea25c1a313baf05b9cc12052
akhil2308 <ch.akhil2308@gmail.com>
2025-04-02 15:40:11 +0530

    added test for health module

---
+++ tests/test_health.py
@@ -0,0 +1,9 @@
+from fastapi.testclient import TestClient
+from main import app
+
+client = TestClient(app)
+
+def test_status_check():
+    response = client.get("/v1/api/health/")
+    assert response.status_code == 200
+    assert response.json() == {"status": "Service is up and running!"}
